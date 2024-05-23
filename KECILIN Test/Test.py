dc0b082223d72235c1c153ad97abe4348333e632
Eric <pengshintsz@pengxincideMacBook-Air.local>
2024-05-23 21:42:38 +0800

    update the code

--- KECILIN Test/Test.py
+++ KECILIN Test/Test.py
@@ -1,37 +1,37 @@
 from imdb import IMDb
 from textblob import TextBlob
 
-# 創建IMDb對象
+# Create IMDb object
 ia = IMDb()
 
-# 搜索一部電影
-movies = ia.search_movie('Dune')  # 你可以替換 'Inception' 為任何你想要查詢的電影
+# Search for a movie
+movies = ia.search_movie('Dune')  # You can replace 'Dune' with any movie you want to search for
 
-# 獲取電影ID
+# Get the movie ID
 movie_id = movies[0].movieID
 
-# 獲取電影詳細信息
+# Get movie details
 movie = ia.get_movie(movie_id)
 
-# 打印電影名稱和簡介
+# Print movie title and plot
 print(f"Movie Title: {movie['title']}")
 
-# 獲取影評
+# Get reviews
 ia.update(movie, 'reviews')
 
-# 檢查是否有影評可用
+# Check if reviews are available
 if 'reviews' in movie:
     reviews = movie['reviews']
     
-    # 打印一則影評並進行情感分析
+    # Print a review and perform sentiment analysis
     if reviews:
-        # 只打印第一則影評
+        # Print only the first review
         review = reviews[0]
         review_content = review.get('content')
         
         print(f"Content: {review_content}\n")
 
-        # 情感分析
+        # Sentiment analysis
         analysis = TextBlob(review_content)
         sentiment = analysis.sentiment
         print(f"Sentiment Analysis:\nPolarity: {sentiment.polarity}, Subjectivity: {sentiment.subjectivity}")
